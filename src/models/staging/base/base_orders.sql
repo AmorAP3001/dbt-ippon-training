4735dcbb9686cf9b214b9c8a263c151c5a129d2e
amor nakouaa <amor.nakouaa.pub@gmail.com>
2025-03-12 13:35:14 +0000

    training

--- src/models/staging/base/base_orders.sql
+++ src/models/staging/base/base_orders.sql
@@ -1,1 +1,8 @@
-SELECT distinct * from {{source ("raw_data","ORDERS")}}
+SELECT distinct
+    identifier::number as identifier
+    , restaurant_identifier::number as restaurant_identifier
+    , parse_json(dishes_ids) as dishes_ids
+    , payment_method::string as payment_method
+    , amount::number(30, 8) as amount
+    , created_at::timestamp as created_at
+     from {{source ("raw_data","ORDERS")}}
