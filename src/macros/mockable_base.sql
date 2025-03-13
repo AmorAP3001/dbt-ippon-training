c7ca132f0e15a159b27d7f6d990ba51a5ed08f90
amor nakouaa <amor.nakouaa.pub@gmail.com>
2025-03-13 11:20:43 +0000

    scenario 7

--- src/macros/mockable_base.sql
+++ src/macros/mockable_base.sql
@@ -1,5 +1,6 @@
 {% macro mockable_base(source_name, table_name) %}
-    {% if target.name == "prod" %} {{ source(source_name, table_name) }}
-    {% else %} {{ ref("sample_" ~ table_name.lower()) }}
+    {% if target.name == "ci" %} {{ ref("sample_" ~ table_name.lower()) }}
+    {% elif target.name == "dev" and var('test_from_sample')=='true' %} {{ ref("sample_" ~ table_name.lower()) }}
+    {% else %} {{ source(source_name, table_name) }}
     {% endif %}
 {% endmacro %}
--- src/models/marts/restaurants__benefit_orders_hourly.sql
+++ src/models/marts/restaurants__benefit_orders_hourly.sql
@@ -1,4 +1,12 @@
-select d.identifier                                           as identifier
+{{
+    config(
+        materialized='incremental',
+        unique_key = ['identifier','hour']
+    )
+}}
+
+select 
+    d.identifier                                           as identifier
     , d.name as name
     , date_trunc('hour',to_timestamp(odf.created_at))      as hour
     , count(*)                                             as nb_ordered
@@ -9,4 +17,8 @@ from
 left join 
     {{ ref('base_dishes') }} as d
         on odf.dishes_id = d.identifier
+{% if is_incremental() %}
+   where odf.created_at >= (select coalesce(max(hour), '1900-01-01') from {{ this }})
+{% endif %}
+
 group by 1,2,3
